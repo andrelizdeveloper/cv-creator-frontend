926a6afde79eba660dd7e44fe63ceb7291b7ead0
Andre Lizaran <andrelizdeveloper@gmail.com>
2026-08-10 01:03:30 -0600

    fix: use globalThis for process.env in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
-const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1];
+const repositoryName = globalThis.process?.env.GITHUB_REPOSITORY?.split('/')[1];
 const githubPagesBase = repositoryName ? `/${repositoryName}/` : '/';
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: process.env.GITHUB_ACTIONS ? githubPagesBase : '/',
+    base: globalThis.process?.env.GITHUB_ACTIONS ? githubPagesBase : '/',
     plugins: [react()],
     test: {
         environment: 'jsdom',
